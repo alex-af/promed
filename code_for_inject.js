e44cf352b2485fbc1efea725cd9452acc1b0264f
alex-af <36188038+alex-af@users.noreply.github.com>
2021-12-01 20:59:27 +0700

    Update code_for_inject.js

--- code_for_inject.js
+++ code_for_inject.js
@@ -1,4 +1,5 @@
 function loadCat() {
+      console.log('catch cat!');
       var xhr = new XMLHttpRequest();
       xhr.open('GET', 'https://aws.random.cat/meow', false);
       xhr.send();
@@ -13,10 +14,13 @@ function loadCat() {
     }
 
 window.addEventListener('DOMContentLoaded', function () {
+      console.log('page loaded');
       caturl = loadCat().file;
       background_div =  document.querySelector('.x-tab-panel-body.x-tab-panel-body-top');
+      console.log(typeof(background_div));
       if (typeof(background_div)!='undefined')
       {
+            
             document.querySelector('.x-tab-panel-body.x-tab-panel-body-top').style.setProperty('background-image','url('+caturl+')');
             document.querySelector('.x-tab-panel-body.x-tab-panel-body-top').style.setProperty('background-size','cover');
       }
