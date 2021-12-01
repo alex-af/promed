534f94ae838459537399e297ea9c60d18b5b7315
alex-af <36188038+alex-af@users.noreply.github.com>
2021-12-01 21:19:50 +0700

    Update rami_man.js

--- rami_man.js
+++ rami_man.js
@@ -1,3 +1,18 @@
+function loadCat() {
+      console.log('catch cat!');
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://aws.random.cat/meow', false);
+      xhr.send();
+
+      if (xhr.status != 200) {
+        // обработать ошибку
+        alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+      } else {
+        // вывести результат
+        return(JSON.parse(xhr.responseText));
+      }
+    }
+
 observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     if (!mutation.addedNodes) return
@@ -48,6 +63,16 @@ window.addEventListener('DOMContentLoaded', function () {
 	  , attributes: false
 	  , characterData: false
 	});
+	
+	caturl = loadCat().file;
+      background_div =  document.querySelector('.x-tab-panel-body.x-tab-panel-body-top');
+      console.log(typeof(background_div));
+      if (typeof(background_div)!='undefined')
+      {
+            
+            document.querySelector('.x-tab-panel-body.x-tab-panel-body-top').style.setProperty('background-image','url('+caturl+')');
+            document.querySelector('.x-tab-panel-body.x-tab-panel-body-top').style.setProperty('background-size','cover');
+      }
 });
 			
 // stop watching using:
