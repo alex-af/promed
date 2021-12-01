8b269ca2660f793dd4aaf7e3dcd4cda06609408c
alex-af <36188038+alex-af@users.noreply.github.com>
2021-12-01 21:22:21 +0700

    Update and rename rami_man_cats.js to rami_man.js

--- rami_man.js
+++ rami_man.js
@@ -1,17 +1,3 @@
-function loadCat() {
-      console.log('catch cat!');
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'https://aws.random.cat/meow', false);
-      xhr.send();
-
-      if (xhr.status != 200) {
-        // обработать ошибку
-        alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
-      } else {
-        // вывести результат
-        return(JSON.parse(xhr.responseText));
-      }
-    }
 
 observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
@@ -64,15 +50,6 @@ window.addEventListener('DOMContentLoaded', function () {
 	  , characterData: false
 	});
 	
-	caturl = loadCat().file;
-      background_div =  document.querySelector('.x-tab-panel-body.x-tab-panel-body-top');
-      console.log(typeof(background_div));
-      if (typeof(background_div)!='undefined')
-      {
-            
-            document.querySelector('.x-tab-panel-body.x-tab-panel-body-top').style.setProperty('background-image','url('+caturl+')');
-            document.querySelector('.x-tab-panel-body.x-tab-panel-body-top').style.setProperty('background-size','cover');
-      }
 });
 			
 // stop watching using:
