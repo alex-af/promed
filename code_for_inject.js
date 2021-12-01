a39bbb9f3144c50036348ae8341bb013fda93e3c
alex-af <36188038+alex-af@users.noreply.github.com>
2021-12-01 20:31:28 +0700

    Update code_for_inject.js

--- code_for_inject.js
+++ code_for_inject.js
@@ -1,1 +1,17 @@
-console.log('test from github');
+function loadCat() {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://aws.random.cat/meow', false);
+      xhr.send();
+
+      if (xhr.status != 200) {
+        // обработать ошибку
+        alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+      } else {
+        // вывести результат
+        return(JSON.parse(xhr.responseText));
+      }
+    }
+
+caturl = loadCat().file;
+document.querySelector('.x-tab-panel-body.x-tab-panel-body-top').style.setProperty('background-image','url('+caturl+')');
+document.querySelector('.x-tab-panel-body.x-tab-panel-body-top').style.setProperty('background-size','cover');
